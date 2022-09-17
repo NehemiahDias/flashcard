9207bb9fb1e659daf35bc1fbb1a63ffc848a87d6
SWELLZ <microsoftdavid4@gmail.com>
2022-09-17 14:09:59 -0700

    add ability to add new flashcards and edit them

--- src/components/flashcardComponents/CreateForm.jsx
+++ src/components/flashcardComponents/CreateForm.jsx
@@ -3,49 +3,93 @@ import { useState } from 'react';
 import './CreateForm.css';
 
 function CreateForm() {
-  const [cards, setCards] = useState([{title: '', definition: ''}]);
+  const [deck, setDeck] = useState({
+    deckName: '',
+    deckDescription: '',
+    deckCards: [],
+  })
+  const [cards, setCards] = useState([{title: '', definition: '', id: 1}]);
+
+  const updateCardTitle = (e, val) => {
+    setCards(current =>
+      current.map((obj, i) => {
+        if (val === obj) {
+          return {...obj, title: e.target.value};
+        }
+        return obj;
+      }),
+    );
+  }
+
+  const updateCardDefinition = (e, val) => {
+    setCards(current =>
+      current.map((obj, i) => {
+        if (val === obj) {
+          return {...obj, definition: e.target.value};
+        }
+        return obj;
+      }),
+    );
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setDeck({...deck, deckCards: cards})
+  }
 
   return (
     <div className="create-deck">
       <h3>Create Deck</h3>
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form onSubmit={handleSubmit}>
           <input 
               placeholder='Study Deck Title...'
+              onChange={e => setDeck({...deck, deckName: e.target.value})}
               className='deck-input'
               required
           />
           <textarea 
             placeholder='Description...' 
+            onChange={e => setDeck({...deck, deckDescription: e.target.value})}
             className='deck-input' 
             rows='5'
           >
           </textarea>
           <div className="new-cards">
-            <div className="card">
-              <div className="card-actions">
-                <div className="num">
-                  <p>1</p>
+            {cards.map((card, index) => 
+            (
+              <div className="card" key={index}>
+                <div className="card-actions">
+                  <div className="num">
+                    <p>{index + 1}</p>
+                  </div>
+                  <div className="delete">
+                    <p>🗑️</p>
+                  </div>
                 </div>
-                <div className="delete">
-                  <p>🗑️</p>
+                <hr />
+                <div className="card-input">
+                  <input 
+                    placeholder='Enter Term'
+                    className='term' 
+                    onChange={e => updateCardTitle(e, card)}
+                    required
+                  />
+                  <p className='term-desc'>Term</p>
+                  <input 
+                    placeholder='Enter Definition'
+                    onChange={e => updateCardDefinition(e, card)}
+                    className='def' 
+                    required
+                  />
+                  <p className='def-desc'>Definition</p>
                 </div>
               </div>
-              <hr />
-              <div className="card-input">
-                <input 
-                  placeholder='Enter Term'
-                  className='term' 
-                />
-                <p className='term-desc'>Term</p>
-                <input 
-                  placeholder='Enter Definition'
-                  className='def' 
-                />
-                <p className='def-desc'>Definition</p>
-              </div>
-            </div>
+            ))}
           </div>
-          <button className='add-card' type='button'>+ Add Card</button>
+          <button className='add-card' type='button' onClick={() => {
+            const newId = cards[cards.length - 1].id + 1
+            setCards([...cards, {title: '', definition: '', id: newId}])
+          }}>+ Add Card</button>
           <button className='submit' type='submit'>Submit</button>
       </form>
     </div>
