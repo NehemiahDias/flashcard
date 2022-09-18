ed11c978e260b3f846e3034604f56bf3be00fb84
Nehemiah Dias <microsoftdavid4@gmail.com>
2022-09-18 10:57:03 -0700

    add ability to toggle whats shown on each card

--- src/components/flashcardComponents/DeckReview.jsx
+++ src/components/flashcardComponents/DeckReview.jsx
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./DeckReview.css";
 
 function DeckReview({ deck }) {
+  const [showedCards, setShowedCards] = useState([...deck.deckCards]);
+
+  useEffect(() => {
+    setShowedCards(
+      showedCards.map((val) => {
+        return { ...val, showingAnswer: false };
+      })
+    );
+  }, []);
+
+  const handleShowAnswer = (item) => {
+    setShowedCards(
+      showedCards.map((prev) => {
+        if (prev === item) {
+          return { ...prev, showingAnswer: prev.showingAnswer ? false : true };
+        }
+        return prev;
+      })
+    );
+  };
+
   return (
     <>
-        <div className="deck-review-intro">
+      <div className="deck-review-intro">
         <h1>This is where you can review your deck!</h1>
-        <p>How to review: Your deck info is going to be listed below, under that will be all your flashcards! When you click on the card, it will show the other side... Just like a real flashcard! It will be ordered in a list randomly each time you refresh the page. I recommend you start from the top and go down from there and refresh to randomize the order. Happy studying!</p>
-        </div>
-        <div className="deck-info">
-            <h1>{deck.deckName}</h1>
-            <p>{deck.deckDescription}</p>
-            {deck.deckCards.map((val, i) => (
-                <div key={i}>
-                    <p>{val.title}</p>
-                    <p>{val.definition}</p>
-                </div>
-            ))}
-        </div>
+        <p>
+          How to review: Your deck info is going to be listed below, under that
+          will be all your flashcards! When you click on the card, it will show
+          the other side... Just like a real flashcard! It will be ordered in a
+          list randomly each time you refresh the page. I recommend you start
+          from the top and go down from there and refresh to randomize the
+          order. Happy studying!
+        </p>
+      </div>
+      <div className="deck-info">
+        <h1>{deck.deckName}</h1>
+        <p>{deck.deckDescription}</p>
+      </div>
+      <div className="showed-cards">
+        {showedCards.map((val, i) => (
+          <button
+            onClick={() => handleShowAnswer(val)}
+            className="flashcard-review"
+            key={i}
+          >
+            {val.showingAnswer ? <p>{val.definition}</p> : <p>{val.title}</p>}
+          </button>
+        ))}
+      </div>
     </>
   );
 }
