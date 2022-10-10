62adf79ed54f498133d846980ad7b64cb1927398
SWELLZ <microsoftdavid4@gmail.com>
2022-10-09 23:05:03 -0700

    add form component for changing password

--- src/components/profileComponents/ChangePassword.jsx
+++ src/components/profileComponents/ChangePassword.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import './ChangePassword.css';
 
 const ChangePassword = ({user}) => {
   return (
-    <div>ChangePassword</div>
+    <div id='change-pass-sect'>
+        <h2>Change Password</h2>
+        <form onSubmit={e => e.preventDefault()}>
+            <label htmlFor='current-pass'>Current Password:</label>
+            <input 
+                id='current-pass'
+                type='password'
+                required
+            />
+            <label htmlFor='new-pass'>Current Password:</label>
+            <input 
+                id='new-pass'
+                type='password'
+                required
+            />
+            <label htmlFor='confirm-new-pass'>Current Password:</label>
+            <input 
+                id='confirm-new-pass'
+                type='password'
+                required
+            />
+            <button type='submit'>Confirm Changes</button>
+        </form>
+    </div>
   )
 }
 
